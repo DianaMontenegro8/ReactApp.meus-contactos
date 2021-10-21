c1fdfc1f123306aa1156b7dfc45f63204bed3998
Diana Montenegro <86521554+DianaMontenegro8@users.noreply.github.com>
2021-10-21 15:32:23 -0300

    inicio da app

---
+++ src/components/Lista.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Lista = () => {
+    return (
+    <div><h3> Lista </h3></div>
+    )
+}
+
+export default Lista
---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import  React from 'react';
+import Lista from '../components/Lista';
+
+
+const App =() => {
+
+  return (
+    <div>
+  
+        <h1>Meus Contactos</h1>
+        <Lista/>
+  
+    </div>
+  )
+}
+
+
+export default App;
